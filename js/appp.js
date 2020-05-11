0b02093e92b6ccd4b90cd0443f38f33a8118830b
thaer <thaerkhaled13@gmail.com>
2020-05-11 16:56:20 +0300

    fixed js code

--- js/appp.js
+++ js/appp.js
@@ -1,50 +1,62 @@
-'use strict'
+ 'use strict'
+ 
+    var name=prompt("please enter your first name");
+    console.log(name);
+    var sName=prompt("please enter your dad name");
+    console.log(sName);
+    var age=prompt("please enter your age");
+    console.log(age);
+    var university=prompt("please enter your university name");
+    console.log(university);
+    var major=prompt("please enter your major");
+    console.log(major);
+    alert(" hi "+ name + "  " + sName +" your age is " + age + " you have graduated from " 
+    + university + " university  with B.D in "+ major );
 
-function login(){
-    var loginfo=confirm('do you have account'); 
-    if (loginfo) {
-        var welcomeOrNot='welcome to your faverate ';
-    } 
-    else{
-        welcomeOrNot='please signUp ';
-    }
-    return welcomeOrNot;
-}
-//document.write(login);
+// function login(){
+//     var loginfo=confirm('do you have account'); 
+//     if (loginfo) {
+//         var welcomeOrNot='welcome to your faverate ';
+//     } 
+//     else{
+//         welcomeOrNot='please signUp ';
+//     }
+//     return welcomeOrNot;
+// }
 
-function helloThere(){
-    var name=prompt('whats your name:');
+
+// function helloThere(){
+//     var name=prompt('whats your name:');
     
 
-    while (name!==null && name!==Number ) {
+//     while (name!==null && name!==Number ) {
 
-       name=prompt('please enter your age');
+//        name=prompt('please enter your age');
         
-    }
-    var numberOf=prompt('haw mutch time you spend sleeping ');
-    console.log(numberOfO);
+//     }
+//     var numberOf=prompt('haw mutch time you spend sleeping ');
+//     console.log(numberOfO);
     
-    for (var i = 0; i< numberOf; i++) {
+//     for (var i = 0; i< numberOf; i++) {
        
-    if (name==='shihab') {
-        photo=photo+'<img src="https://migrationology.com/wp-content/uploads/2016/04/mansaf-jordanian-food.jpg">';
+//     if (name==='shihab') {
+//         photo=photo+'<img src="https://migrationology.com/wp-content/uploads/2016/04/mansaf-jordanian-food.jpg">';
 
         
-    } else if (name==='razan') {
-         photo=photo+'<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRNCBvzqVUTtPRnqtRh5agxHcraTtXHJlOxA8eypYP8HUSPDNTo&usqp=CAU">';
+//     } else if (name==='razan') {
+//          photo=photo+'<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRNCBvzqVUTtPRnqtRh5agxHcraTtXHJlOxA8eypYP8HUSPDNTo&usqp=CAU">';
 
-    } else if (name==='thaer') {
+//     } else if (name==='thaer') {
 
-        photo=photo+'<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRDRyrq8TdpdG7nW_1ddNaOlxRBZaKhTrHdCqjCc2zlkmI1fHHj&usqp=CAU">';
-   } 
-   //else {
-    //    photo='please chose mansaf or shawerma or falafel ';
-}   
+//         photo=photo+'<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRDRyrq8TdpdG7nW_1ddNaOlxRBZaKhTrHdCqjCc2zlkmI1fHHj&usqp=CAU">';
+//    } 
+ 
+// }   
 
    
     
 
-    return photo;
+//     return photo;
     
-    //document.write(foodOrder());
-}
+   
+// }
