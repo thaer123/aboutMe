cde1d0b9e76aa413c27b8da817116ba8419f1c57
thaer <thaerkhaled13@gmail.com>
2020-05-15 22:23:25 +0300

    made some shanged on css file

--- js/appp.js
+++ js/appp.js
@@ -1,123 +1,123 @@
-  'use strict'
-var score = 0;
-var name = prompt('What is your name?');
-console.log('User name: ' + name);
+//   'use strict'
+// var score = 0;
+// var name = prompt('What is your name?');
+// console.log('User name: ' + name);
 
-alert('Hi ' + name + ', Welcome to my page!');
-alert('lets play a guessing game about me. Only Yes or No answers are valid.');
+// alert('Hi ' + name + ', Welcome to my page!');
+// alert('lets play a guessing game about me. Only Yes or No answers are valid.');
 
-function yesCorrect (question) {
-    if (question.toUpperCase() === 'YES') {
-        score++;
-        alert("Yes!, you are right.");
-    }
-    else if (question.toUpperCase() === 'NO'){
-        alert("Sorry, wrong answer.");
-    }
-    else {
-        alert("This answer is not valid");
-    }
-}
+// function yesCorrect (question) {
+//     if (question.toUpperCase() === 'YES') {
+//         score++;
+//         alert("Yes!, you are right.");
+//     }
+//     else if (question.toUpperCase() === 'NO'){
+//         alert("Sorry, wrong answer.");
+//     }
+//     else {
+//         alert("This answer is not valid");
+//     }
+// }
 
-function noCorrect (question) {
-    if (question.toUpperCase() === 'YES') {
-        alert("Sorry, wrong answer.");
-    }
-    else if (question.toUpperCase() === 'NO'){
-        score++;
-        alert("Yes! you are right.");
-    }
-    else {
-        alert("This answer is not valid");
-    }
-}
-
-
-// Guess Question 1
-var favColor = prompt(name + " Is my favorite color Red? Yes or No:");
-console.log('my favorite color: ' + favColor);
-
-noCorrect(favColor);
+// function noCorrect (question) {
+//     if (question.toUpperCase() === 'YES') {
+//         alert("Sorry, wrong answer.");
+//     }
+//     else if (question.toUpperCase() === 'NO'){
+//         score++;
+//         alert("Yes! you are right.");
+//     }
+//     else {
+//         alert("This answer is not valid");
+//     }
+// }
 
 
-// Guess Question 2
-var coffee = prompt(name + ' Do I like coffee? Yes or No:');
-console.log('coffee: ' + coffee);
+// // Guess Question 1
+// var favColor = prompt(name + " Is my favorite color Red? Yes or No:");
+// console.log('my favorite color: ' + favColor);
 
-noCorrect(coffee);
-
-
-// Guess Question 3
-var rollerCoaster = prompt(name + ' Do I like roller coasters? Yes or No');
-console.log('roller coaster: ' + rollerCoaster);
-
-noCorrect(rollerCoaster);
-
-// Guess Question 4
-var macUser = prompt(name + ' Do I prefer Mac computers? Yes or No');
-console.log('mac user: ' + macUser);
-
-yesCorrect(macUser);
+// noCorrect(favColor);
 
 
-// Guess Question 5
-var windowsUser = prompt(name + ' Do I prefer Windows computers? Yes or No');
-console.log('windows user: ' + windowsUser);
+// // Guess Question 2
+// var coffee = prompt(name + ' Do I like coffee? Yes or No:');
+// console.log('coffee: ' + coffee);
 
-noCorrect(windowsUser);
-
-
-// Guess Question 6
-function guessFavNumber() {
-    var counter = 1;
-    if (counter === 5) {
-        alert('Sorry you run out of guesses');
-    }
-    while (counter < 5 && number !==27) {
-        var number = parseInt(prompt('Guess my favorite number'));
-        if (number > 27) {
-            alert('You guessed too high! Guess again');
-            counter++;
-        } else if (number < 27) {
-            alert('You guessed too low! Guess again');
-            counter++;
-        }
-    }
-    if (number === 27) {
-        alert('That is correct! My favorite number is 27!');
-        score++;
-        counter = 5;
-    }
-}
-
-guessFavNumber();
+// noCorrect(coffee);
 
 
-// Guess Question 7
+// // Guess Question 3
+// var rollerCoaster = prompt(name + ' Do I like roller coasters? Yes or No');
+// console.log('roller coaster: ' + rollerCoaster);
 
-function questionSeven() {
-    var userFavFruit = ['apple', 'strawberry', 'orange', 'grape', 'watermelon'];
+// noCorrect(rollerCoaster);
 
-    for (var i = 0; i < 6; i++) {
-        var userAnsw = prompt('Can you guess my favorite fruit?');
-        var guess = userAnsw.toLowerCase();
+// // Guess Question 4
+// var macUser = prompt(name + ' Do I prefer Mac computers? Yes or No');
+// console.log('mac user: ' + macUser);
 
-        for (var j = 0; j < userFavFruit.length; j++){
-            if (guess === userFavFruit[j]) {
-                alert('Congrats you got it right!!!');
-                score++;
-                i = 6;
-                break;
-            }
-        }
-        if (i !==6){
-            alert('Sorry wrong answer, try again');
-        }
-    }
-}
+// yesCorrect(macUser);
 
-questionSeven();
 
-alert('My favorite fruits are: apple, strawberry, orange, grape and watermelon');
+// // Guess Question 5
+// var windowsUser = prompt(name + ' Do I prefer Windows computers? Yes or No');
+// console.log('windows user: ' + windowsUser);
 
-alert('Good Job!... You got ' + score + ' correct answers!');
+// noCorrect(windowsUser);
+
+
+// // Guess Question 6
+// function guessFavNumber() {
+//     var counter = 1;
+//     if (counter === 5) {
+//         alert('Sorry you run out of guesses');
+//     }
+//     while (counter < 5 && number !==27) {
+//         var number = parseInt(prompt('Guess my favorite number'));
+//         if (number > 27) {
+//             alert('You guessed too high! Guess again');
+//             counter++;
+//         } else if (number < 27) {
+//             alert('You guessed too low! Guess again');
+//             counter++;
+//         }
+//     }
+//     if (number === 27) {
+//         alert('That is correct! My favorite number is 27!');
+//         score++;
+//         counter = 5;
+//     }
+// }
+
+// guessFavNumber();
+
+
+// // Guess Question 7
+
+// function questionSeven() {
+//     var userFavFruit = ['apple', 'strawberry', 'orange', 'grape', 'watermelon'];
+
+//     for (var i = 0; i < 6; i++) {
+//         var userAnsw = prompt('Can you guess my favorite fruit?');
+//         var guess = userAnsw.toLowerCase();
+
+//         for (var j = 0; j < userFavFruit.length; j++){
+//             if (guess === userFavFruit[j]) {
+//                 alert('Congrats you got it right!!!');
+//                 score++;
+//                 i = 6;
+//                 break;
+//             }
+//         }
+//         if (i !==6){
+//             alert('Sorry wrong answer, try again');
+//         }
+//     }
+// }
+
+// questionSeven();
+
+// alert('My favorite fruits are: apple, strawberry, orange, grape and watermelon');
+
+// alert('Good Job!... You got ' + score + ' correct answers!');
